42de96b78545a35925095272b3c11409d3d98daf
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2025-01-13 01:25:06 +0530

    break and continue

--- 6_break_continue/1_break_or_continue.go
+++ 6_break_continue/1_break_or_continue.go
@@ -4,15 +4,41 @@ import "fmt"
 
 func main() {
 
+	//break
+	fmt.Println("break statement")
+
 	for i := 0; i <= 10; i++ {
 
+		if i == 5 {
+			break
+		}
 		fmt.Println(i)
-		break
+	}
+	fmt.Println("continue statement")
+
+	//continue
+	for i := 0; i <= 10; i++ {
+		if i%2 != 0 {
+			continue
+		}
 		fmt.Println(i)
 	}
 }
 
 /*
 op:
+break statement
 0
+1
+2
+3
+4
+continue statement
+0
+2
+4
+6
+8
+10
+
 */
