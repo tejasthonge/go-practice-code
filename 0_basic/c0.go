34a7c5bdde4f71373a2bffa71eb0b17da656bce3
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2025-01-12 17:10:09 +0530

    cleaning

---
+++ 0_basic/c0.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Jay Shree ran") // this is come form the fmt package
+	println("amar")
+	
+}
+
---
+++ 1_simple_values/c0.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//string literals
+	fmt.Println("This is smaple String")
+	//int
+	fmt.Println(22)
+	//float
+	fmt.Println(323.232)
+	fmt.Println(2342.32 / 32)
+	//boolean
+	fmt.Println(true)
+
+}
